3154c6f367a8c413137877b8110b15b714978b0e
Curtis <hong.curtis@gmail.com>
2022-01-22 14:45:32 -0800

    update to use fs instead of importing db'

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,24 +1,34 @@
-const router = require('express').Router();
-const {createNewNote, deleteNote} = require('../../lib/notes')
-const {notes} = require('../../db/db.json');
-const res = require('express/lib/response');
-const { sendFile } = require('express/lib/response');
+const router = require("express").Router();
+const { createNewNote, deleteNote } = require("../../lib/notes");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
+router.get("/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf-8", (err, notes) => {
+    let notesParsed = JSON.parse(notes);
+    return res.json(notesParsed);
+  });
+});
 
-router.get('/notes', (req, res)=>{
-    res.json(notes);
-})
+router.post("/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf-8", (err, notes) => {
+    let notesParsed = JSON.parse(notes);
+    req.body.id = uuidv4();
+    notesParsed.push(req.body);
+    fs.writeFile("db/db.json", JSON.stringify(notesParsed), (err, newNotes) => {
+      return res.json(newNotes);
+    });
+  });
+});
 
-router.post('/notes', (req,res)=>{
-    // set id based on what the next index of the array will be 
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-})
+router.delete("/notes/:id", (req, res) => {
+  deleteNote(req.params.id, notes);
+  res
+    .json(notes)
+    .catch((err) => {
+      res.status(500).json(err);
+    })
+    .reload();
+});
 
-router.delete('/notes/:id',(req,res)=>{
-    deleteNote(req.params.id,notes);
-    res.json(notes)
-})
-
-module.exports = router
+module.exports = router;
