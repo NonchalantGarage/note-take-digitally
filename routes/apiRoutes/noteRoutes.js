4d005e136786d9aa83bb80a7e8eff6837d4add65
Curtis <hong.curtis@gmail.com>
2022-01-21 22:04:48 -0800

    added a post route that uses a function from the index.js in the lib/ directory

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const {createNewNote} = require('../../lib/notes')
-const {notes} = require('../../db/db.json')
+const {notes} = require('../../db/db.json');
+const res = require('express/lib/response');
+const { sendFile } = require('express/lib/response');
 
 
-// router.get('/notes'(req, res)=>{
+router.get('/notes', (req, res)=>{
+    res.json(notes);
+})
 
-// })
-
+router.post('/notes', (req,res)=>{
+    // set id based on what the next index of the array will be 
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+})
 
 module.exports = router
